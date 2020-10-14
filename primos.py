9b8217fb167a8fd22c334985f66459d9232e5a6f
igordzc <igor.camargo@aluno.faculdadeimpacta.com.br>
2020-10-13 21:21:12 -0300

    ac3

--- primos.py
+++ primos.py
@@ -15,18 +15,18 @@ def nao_entre_em_panico():
 
     primos = "2,"
 
-    while p< limite:
+    while p < limite:
         ehprimo = 1
         for i in range(2, numero):
             if numero % i == 0:
                 ehprimo = 0
                 break
-            if (ehprimo):
-                primos = primos + str(numero) + ","
-                p += 1
-                if(p % 10 == 0):
-                    primos = primos + "<br>"
-            numero+=1
+        if (ehprimo):
+            primos = primos + str(numero) + ","
+            p += 1
+            if(p % 10 == 0):
+                primos = primos + "<br>"
+        numero+=1
 
     return primos
 
